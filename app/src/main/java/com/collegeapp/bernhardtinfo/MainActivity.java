506151a18f17aedaa26cab8e5688cb2fbfc7ed93
sandipbhandari <bhandarisandip882@gmail.com>
2022-03-07 20:07:15 +0545

    used firebase

--- app/src/main/java/com/collegeapp/bernhardtinfo/MainActivity.java
+++ app/src/main/java/com/collegeapp/bernhardtinfo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.collegeapp.bernhardtinfo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView uploadnotice=findViewById(R.id.uploadnoticebtn);
+        TextView uploadnoticetxt=findViewById(R.id.uploadnotice);
+
+        uploadnoticetxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+           Intent intent=new Intent(MainActivity.this,uploadnotice.class);
+            }
+        });
+
+        uploadnotice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,uploadnotice.class);
+                startActivity(intent);
+            }
+        });
     }
 }
