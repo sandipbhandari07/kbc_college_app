05eaa6f93fb20a6b157a71d7fc7e81c962028be0
sandipbhandari <bhandarisandip882@gmail.com>
2022-03-08 13:41:41 +0545

    can add image from gallery

--- app/src/main/java/com/collegeapp/bernhardtinfo/MainActivity.java
+++ app/src/main/java/com/collegeapp/bernhardtinfo/MainActivity.java
@@ -1,35 +1,36 @@
 package com.collegeapp.bernhardtinfo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    CardView uploadnotice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView uploadnotice=findViewById(R.id.uploadnoticebtn);
-        TextView uploadnoticetxt=findViewById(R.id.uploadnotice);
+        uploadnotice = findViewById(R.id.addNotice);
+        uploadnotice.setOnClickListener(this);
 
-        uploadnoticetxt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-           Intent intent=new Intent(MainActivity.this,uploadnotice.class);
-            }
-        });
 
-        uploadnotice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,uploadnotice.class);
-                startActivity(intent);
-            }
-        });
+    }
+    @Override
+    public void onClick(View view) {
+     switch (view.getId()){
+         case R.id.addNotice:
+             Intent intent= new Intent(MainActivity.this,uploadnotice.class);
+             startActivity(intent);
+             break;
+     }
     }
 }
--- app/src/main/java/com/collegeapp/bernhardtinfo/uploadnotice.java
+++ app/src/main/java/com/collegeapp/bernhardtinfo/uploadnotice.java
@@ -1,13 +1,65 @@
 package com.collegeapp.bernhardtinfo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import java.io.IOException;
+import java.net.URI;
 
 public class uploadnotice extends AppCompatActivity {
+
+    private CardView addimages1;
+    private ImageView noticeimageview;
+
+    private final int REQ = 1;
+
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uploadnotice);
+
+        addimages1 = findViewById(R.id.addNotice1234);
+        noticeimageview = findViewById(R.id.noticeImageViewPrv);
+
+
+        addimages1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+            }
+        });
+    }
+
+    private void openGallery() {
+        Intent pickimage = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickimage, REQ);
+    }
+
+ @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQ && resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            noticeimageview.setImageBitmap(bitmap);
+
+        }
     }
 }
