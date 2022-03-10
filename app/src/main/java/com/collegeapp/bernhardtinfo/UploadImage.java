b9a3e8e3eda324c8f3da0bc22f102605a7c45ddd
sandipbhandari <bhandarisandip882@gmail.com>
2022-03-10 20:52:01 +0545

    upload image

---
+++ app/src/main/java/com/collegeapp/bernhardtinfo/UploadImage.java
@@ -0,0 +1,14 @@
+package com.collegeapp.bernhardtinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UploadImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_image);
+    }
+}
