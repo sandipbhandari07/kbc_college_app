fc37cd60328630cfdb8daee5d45e3558098ece3c
sandipbhandari <bhandarisandip882@gmail.com>
2022-03-20 20:18:44 +0545

    faculty

---
+++ app/src/main/java/com/collegeapp/bernhardtinfo/faculty/AddTeachers.java
@@ -0,0 +1,16 @@
+package com.collegeapp.bernhardtinfo.faculty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.collegeapp.bernhardtinfo.R;
+
+public class AddTeachers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_teachers);
+    }
+}
---
+++ app/src/main/java/com/collegeapp/bernhardtinfo/faculty/updatefaculty.java
@@ -0,0 +1,15 @@
+package com.collegeapp.bernhardtinfo.faculty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.collegeapp.bernhardtinfo.R;
+
+public class updatefaculty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updatefaculty);
+    }
+}
